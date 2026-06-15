a2a0e24f7dc91c9ca9a3e4a753da963bc9322af9
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-06-15 22:06:47 +0200

    test: add test to ensure overlay consumption works
    This already fixed one issue where the building building function was secretly
    the fetching function because I copied and pasted something without changing
    it.

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,17 +4,61 @@
 }:
 let
   fetchHelper = inputs.self.lib.fetchQmkFirmwareHelperPath;
-  buildHelper = inputs.self.lib.fetchQmkFirmwareHelperPath;
+  buildHelper = inputs.self.lib.buildQmkFirmwareHelperPath;
 in
-{
-  flake.overlays.default =
-    final: previous:
-    (
-      rec {
-        qmkFirmware = inputs.self.firmware.${final.stdenv.system};
-        fetchQmkFirmware = previous.callPackage fetchHelper { };
-        buildQmkFirmware = previous.callPackage buildHelper { };
-      }
-      // inputs.self.packages.${final.stdenv.system}
-    );
+rec {
+  flake.overlays.default = final: previous: rec {
+    qmkFirmware = inputs.self.firmware.${final.stdenv.system};
+    fetchQmkFirmware = previous.callPackage fetchHelper { };
+    buildQmkFirmware = previous.callPackage buildHelper { };
+  };
+
+  perSystem =
+    {
+      system,
+      ...
+    }:
+    let
+      pkgs' = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          flake.overlays.default
+        ];
+      };
+    in
+    {
+      checks.overlays =
+        pkgs'.runCommand "test-fetch-qmk-firmware"
+          rec {
+            nativeBuildInputs = with pkgs'; [ jq ];
+
+            fetched = pkgs'.fetchQmkFirmware {
+              hash = "sha256-3otHYlL7IYS3VFd5/S/mBFUcZkT3Fvxo87l3VTDxbL0=";
+              clearKeyboards = false;
+              tag = "0.33.3";
+            };
+
+            build = pkgs'.buildQmkFirmware {
+              qmkFirmware = fetched;
+              keyboard = "planck/rev6";
+              split = true;
+            };
+
+            meta = with pkgs'.lib; {
+              description = "Tests for the `overlay` helper function.";
+              maintainers = with maintainers; [
+                Tygo-van-den-Hurk
+              ];
+            };
+          }
+          /* SHELL */ ''
+            printf "The build firmware paths are: "
+            jq -n \
+              --arg out "$out" \
+              --arg fetched "$fetched" \
+              --arg build "$build" \
+              '$ARGS.named'
+            touch $out
+          '';
+    };
 }
