b3608418961deabf30c0b434ea3ab5c59e1c378c
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-06-17 12:08:27 +0200

    chore(shell): enable pre-commit hooks
    Those were not enabled and broken, but since those are fixed we can now safely
    enable them.

--- shells/development.nix
+++ shells/development.nix
@@ -1,15 +1,32 @@
 {
-  perSystem = { pkgs, ... }: {
-    devShells."development" =
-      with pkgs;
-      let
-        packages = [
-          qmk
-          git
-        ];
-      in
-      mkShell {
-        inherit packages;
+  perSystem =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    let
+      formatters = builtins.attrValues config.treefmt.build.programs;
+      hooks = config.pre-commit.settings.enabledPackages;
+      packages = with pkgs; [
+        act # Run your GitHub Actions locally
+        git # Distributed version control system
+        qmk # Compile QMK firmware
+      ];
+
+      buildInputs = packages ++ formatters ++ hooks;
+
+      shellHook = ''
+        ${config.pre-commit.shellHook}
+        if [ -f .env ]; then
+          source .env
+        fi
+      '';
+    in
+    {
+      devShells."development" = pkgs.mkShell {
+        inherit buildInputs;
+        inherit shellHook;
       };
-  };
+    };
 }
