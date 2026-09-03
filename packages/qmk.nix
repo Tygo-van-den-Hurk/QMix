c11b49a6fa694e9d4f441875a3ecc3a12acfbbc4
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-09-03 13:52:34 +0200

    fix: overwrote python version to 3.13 for qmk
    There are some issues with deprecated code.

--- packages/qmk.nix
+++ packages/qmk.nix
@@ -1,9 +1,14 @@
 {
   perSystem = { pkgs, ... }: {
-    packages.qmk = pkgs.qmk.overrideAttrs (oldAttrs: {
-      propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
-        pkgs.python3Packages.appdirs
-      ];
-    });
+    packages.qmk =
+      (pkgs.qmk.override {
+        # use python 3.13, because some required code is deprecated and removed
+        python3 = pkgs.python313;
+      }).overrideAttrs
+        (oldAttrs: {
+          propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
+            pkgs.python313Packages.appdirs
+          ];
+        });
   };
 }
